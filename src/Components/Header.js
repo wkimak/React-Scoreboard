1f57bfe262114ae7e77885e22166bdb3ee96f374
William Kimak <wkimak94@gmail.com>
2017-12-10 12:31:06 -0500

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Header(props){
+	
+
+	return(
+      <div className="header">
+        <h2> {props.title}</h2>
+      </div>
+		);
+}
+
+export default Header;
